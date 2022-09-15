4ebda6eff354c0e7e785dec94fa8d06f19c82937
Dmitry Mordanov <mordanov314@gmail.com>
2022-09-15 23:35:18 +0300

    task 1, 3, 4 complete

---
+++ task03.py
@@ -0,0 +1,56 @@
+# Создайте программу для игры в ""Крестики-нолики"".
+
+# Хранить все будем в одномерном массиве
+field = '123456789'
+3
+def PrintField(local_field): # функция печати состояния игрового поля
+    print('-' * 13)
+    for i in range(3): # печатаем целыми строками
+        print(f'| {local_field[i*3]} | {local_field[i*3 + 1]} | {local_field[i*3 + 2]} |')
+        print('-' * 13)
+
+def MyInput(local_field): # функция ввода нового элемента (с проверками)
+    num = int(input('Введите позию, куда хотите сходить 1..9: ')) # проверку на нецифру делать не стал
+    while True:
+        if not ((0 < num) and (num < 10)):
+            print('Ввели неправильное место на поле')
+        elif not(local_field[num-1] in '123456789'): # если поле занято, то вместо цифры там X или O
+            print('Это поле уже занято')
+        else: return str(num)
+        num = int(input('Введите позию, куда хотите сходить 1..9: '))
+
+def MyCheckGame(local_field): # функция проверки окончания игры
+    win_patterns = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)) # если в этих позициях
+                                                                                     # стоят XXX или OOO,
+                                                                                     # то кто-то выйграл
+    if len(local_field.replace('X','').replace('O','')) == 0: return 'Победила дружба! Ничья!'
+    if 'XXX' in list(filter(lambda x: x,[local_field[win_patterns[i][0]]+\
+                                         local_field[win_patterns[i][1]]+\
+                                         local_field[win_patterns[i][2]] \
+                                    for i in range(len(win_patterns))])):
+        return 'XXX Победили крестики XXX'
+    if 'OOO' in list(filter(lambda x: x,[local_field[win_patterns[i][0]]+\
+                                         local_field[win_patterns[i][1]]+\
+                                         local_field[win_patterns[i][2]] \
+                                    for i in range(len(win_patterns))])):
+        return 'OOO Победили нулики OOO'
+    return 'None'
+
+print(' Крестики - нолики (без ИИ, играют только человеки)')
+game_result = 'None'
+x_turn = True # Х - ходят первые
+
+while game_result == 'None':
+    PrintField(field)
+    if x_turn:
+        print('Ходят X')
+        field = field.replace(MyInput(field),'X')
+    else:
+        print('Ходят O')
+        field = field.replace(MyInput(field),'O')
+    game_result = MyCheckGame(field)
+    x_turn = not x_turn
+
+print('Игра окончена')
+PrintField(field)
+print(game_result)
---
+++ task04.py
@@ -0,0 +1,41 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+def RleZip(text): # Функция сжатия
+    if len(text) < 1: return ''
+    s = ''
+    prev_char= text[0] # тут храним пердыдущий символ в тексте
+    count = 0 # счетчик числа вхождения символа в исходном тексте
+    for char in text:
+        if prev_char != char: # символ поменялся - поэтому записываем его
+            s += prev_char+str(count) # пишем символ, и сколько раз он встретился
+            prev_char = char
+            count = 1 # новый символ и уже встретился 1 раз
+        else:
+            if count == 9:  # т.к. после одного символа стоит только одна цифра - поэтому
+                            #  больше 9 символов подряяд не делаем.
+                s += prev_char+str(count) # грузим сжатый символ в строку
+                count = 1
+            else:
+                count += 1
+    s += char+str(count) # обрабатываем последный символ
+    return s
+
+def RleUnZip(text): #  функция распаковки файла
+    ret = [text[i:i+2] for i in range(0, len(text), 2)] # делим текст на список по 2 символа
+    return "".join(ret[i][0]*int(ret[i][1]) for i in range(len(ret))) # пишем строку по i символов
+                                                                      # подряд для каждой пары
+
+with open('kolobok_orig.txt','r', encoding='UTF-8') as f: # читаем оригинальный файл
+    text = f.read()
+
+with open('kolobok.rle','w', encoding='UTF-8') as f: # Сжимаем текст и пишем его в файл
+    f.write(RleZip(text))
+
+with open('kolobok.rle','r', encoding='UTF-8') as f: #  Читаем сжатый файл и расжимаем его
+    text = RleUnZip(f.read())
+
+with open('kolobok_unzip.txt','w', encoding='UTF-8') as f: # Пишем расжатый текст в новый файл
+    f.write(text)
+
+# плохо жмет - в тексте мало одинаковых символов подряд идет... вот картинки всякие - другое дело
